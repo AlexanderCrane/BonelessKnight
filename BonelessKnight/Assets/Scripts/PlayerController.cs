e0460ea92babb4126a97328bba3a05f52d9d8641
AlexanderCrane <InnovateTheWorld@yahoo.com>
2024-01-26 15:04:18 -0600

    Player animation controller for idle and run, also added wizard

--- BonelessKnight/Assets/Scripts/PlayerController.cs
+++ BonelessKnight/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public Rigidbody hips;
     public bool isGrounded = true;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +20,29 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate() 
     {
 
+        if(Mathf.Abs(hips.velocity.magnitude) > 1)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+
         // Note: ALL of the transform directions (transform.up, etc) are "incorrect" because the rig is set up this way and we aren't taking the time to fix it at the moment
 
         if(Input.GetKey(KeyCode.W))
         {
             if(Input.GetKey(KeyCode.LeftShift))
             {
-                hips.AddForce(hips.transform.up * (speed * 1.5f));
+                Vector3 cameraFacingWithoutX = Vector3.Normalize(new Vector3(Camera.main.transform.forward.x, Camera.main.transform.position.y, Camera.main.transform.forward.z));
+                hips.AddForce(cameraFacingWithoutX * (speed * 1.5f));
 
             }
             else
             {
-                hips.AddForce(hips.transform.up * speed);    
+                Vector3 cameraFacingWithoutX = Vector3.Normalize(new Vector3(Camera.main.transform.forward.x, Camera.main.transform.position.y, Camera.main.transform.forward.z));
+                hips.AddForce(cameraFacingWithoutX * speed);    
             }
         }
 
@@ -41,7 +53,8 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKey(KeyCode.S))
         {
-            hips.AddForce(-hips.transform.up * speed);
+            Vector3 cameraFacingWithoutX = Vector3.Normalize(new Vector3(Camera.main.transform.forward.x, Camera.main.transform.position.y, Camera.main.transform.forward.z));
+            hips.AddForce(-cameraFacingWithoutX * speed);
         }
 
         if(Input.GetKey(KeyCode.D))
